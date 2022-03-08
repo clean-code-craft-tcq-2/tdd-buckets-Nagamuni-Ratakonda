5ad37fe29f9a888a7eea87919f10d7fd406c589d
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-03-08 20:47:44 +0530

    Update CaptureChargingCurrentRanges.c

--- CaptureChargingCurrentRanges.c
+++ CaptureChargingCurrentRanges.c
@@ -6,18 +6,18 @@ int CaptureChargingCurrentRanges (int *CurrentRanges, int TotalCurrentRanges){
   int i,j,swap,lowerRange,NextElement,RangeDifference;
   int TotalPeriodicSetRanges = 0;
   
-  for(i = 1; i <= (TotalCurrentRanges-1); i++)
-  {
-    j = i;
-    while(j > 0 && CurrentRanges[j-1] > CurrentRanges[j])
-    {
-      swap = CurrentRanges[j];
-      CurrentRanges[j] = CurrentRanges[j-1];
-      CurrentRanges[j-1] = swap;
-      j--;
-      printf("sorted");
-    }
-  }
+//   for(i = 1; i <= (TotalCurrentRanges-1); i++)
+//   {
+//     j = i;
+//     while(j > 0 && CurrentRanges[j-1] > CurrentRanges[j])
+//     {
+//       swap = CurrentRanges[j];
+//       CurrentRanges[j] = CurrentRanges[j-1];
+//       CurrentRanges[j-1] = swap;
+//       j--;
+//       printf("sorted");
+//     }
+//   }
   
   
   for (i = 0; i < TotalCurrentRanges ; i++) 
