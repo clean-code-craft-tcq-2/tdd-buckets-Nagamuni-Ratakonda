ae5c739d6f630cb4dd22e3fbe69d250f5bc674d5
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-03-08 21:25:37 +0530

    Update CaptureChargingCurrentRanges.c

--- CaptureChargingCurrentRanges.c
+++ CaptureChargingCurrentRanges.c
@@ -3,38 +3,34 @@
 
 void swapRangeSets(int *CurrentRanges)
 {
-  int i,j,swap;
-  if(CurrentRanges[i] > CurrentRanges[j])
+  int i,swap;
+  if(CurrentRanges[i] > CurrentRanges[i+1])
   {
         swap = CurrentRanges[i];
-        CurrentRanges[i] = CurrentRanges[j];
-        CurrentRanges[j] = swap;
+        CurrentRanges[i] = CurrentRanges[j+1];
+        CurrentRanges[j+1] = swap;
         printf("sorted");
   }
 }
 
 void SortCurrentRanges(int *CurrentRanges, int TotalCurrentRanges)
 {
-  int i,j;
+  int i,j,swap;
   
-  for(i = 0; i <TotalCurrentRanges; ++i)
+  for(i = 0; i <(TotalCurrentRanges - 1); i++)
   {
-    for(j = i+1; j < TotalCurrentRanges; ++j)
-    {
-      swapRangeSets(CurrentRanges);
-    }
+    swapRangeSets(CurrentRanges);
+    i = -1;
   }
 }
 
 int CheckForPeriodicSetRanges(int *CurrentRanges, int TotalCurrentRanges)
 {
-  int i,j,lowerRange,NextElement,RangeDifference;
+  int lowerRange,NextElement,RangeDifference;
   int TotalPeriodicSetRanges = 0;
-  
-  for (i = 0; i < TotalCurrentRanges ; i++) 
-  {
-    lowerRange = CurrentRanges[i];
-    NextElement = CurrentRanges[i+1];
+ 
+    lowerRange = CurrentRanges[0];
+    NextElement = CurrentRanges[1];
     RangeDifference = NextElement - lowerRange;
     if(RangeDifference == 0 || RangeDifference == 1)
     {
@@ -42,7 +38,6 @@ int CheckForPeriodicSetRanges(int *CurrentRanges, int TotalCurrentRanges)
       printf("Range, Readings");
       printf("%d-%d, %d",lowerRange,NextElement,TotalCurrentRanges);
     }
-  }
   
   return TotalPeriodicSetRanges;
 }
@@ -51,7 +46,11 @@ int CaptureChargingCurrentRanges (int *CurrentRanges, int TotalCurrentRanges){
   int TotalPeriodicSetRanges = 0;
   
   SortCurrentRanges(CurrentRanges,TotalCurrentRanges);
-  TotalPeriodicSetRanges = CheckForPeriodicSetRanges(CurrentRanges,TotalCurrentRanges);
+  
+  for(i = 0; i < TotalCurrentRanges ; i++)
+  {
+   TotalPeriodicSetRanges = CheckForPeriodicSetRanges(CurrentRanges,TotalCurrentRanges);
+  }
   
   return TotalPeriodicSetRanges;
 }
