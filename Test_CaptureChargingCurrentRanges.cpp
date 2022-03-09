09fe13276021b03842688bb1705e7b18b3c0a26b
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-03-09 10:46:34 +0530

    Update Test_CaptureChargingCurrentRanges.cpp

--- Test_CaptureChargingCurrentRanges.cpp
+++ Test_CaptureChargingCurrentRanges.cpp
@@ -17,3 +17,8 @@ TEST_CASE("Checks the charging current of same ranges and captures the no of occ
   int CurrentRanges[] = {4,4};
   REQUIRE(CaptureChargingCurrentRanges(CurrentRanges,2) == 1);
 }
+
+TEST_CASE("Checks the charging current of same ranges and captures the no of occurences") {
+  int CurrentRanges[] = {3,3,5,4,10,11};
+  REQUIRE(CaptureChargingCurrentRanges(CurrentRanges,5) == 2);
+}
