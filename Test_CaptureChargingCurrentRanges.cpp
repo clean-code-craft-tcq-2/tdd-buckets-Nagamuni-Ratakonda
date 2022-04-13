81aaf4df844d355e6ee871639c72a4c26b1d31ca
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-13 14:36:49 +0530

    Update Test_CaptureChargingCurrentRanges.cpp

--- Test_CaptureChargingCurrentRanges.cpp
+++ Test_CaptureChargingCurrentRanges.cpp
@@ -17,3 +17,20 @@ TEST_CASE("Checks the charging current of same ranges and captures the no of occ
   int CurrentRanges[] = {4,4};
   REQUIRE(CaptureChargingCurrentRanges(CurrentRanges,2) == 1);
 }
+
+//Testcases for A2DConverter
+TEST_CASE("Testcase to cehck whether Analog current ranges converted to Digital") {
+  int AnalogCurrentRanges[] = {4094, 819,3790,1230,1640,2870};
+  int TotalNoOfCurrentRanges = sizeof(AnalogCurrentRanges) / sizeof(AnalogCurrentRanges[0]);
+  float DigitalCurrentRanges[TotalNoOfCurrentRanges];
+  float ExpectedCurrentRangesInAmps[] = {10, 2, 9, 3, 4 , 7};
+  
+  
+  REQUIRE(ConvertAnalogCurrentSamplesToDigital() == true);
+  
+  for(int rangeIndex = 0; rangeIndex < TotalNoOfCurrentRanges; rangeIndex++)
+  {
+    REQUIRE(DigitalCurrentRanges[i] == ExpectedCurrentRangesInAmps[i]);
+  }
+
+}
