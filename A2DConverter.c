ae408fb50797f1867fa37d4210040811c9eeffcc
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-13 15:20:43 +0530

    Update A2DConverter.c

--- A2DConverter.c
+++ A2DConverter.c
@@ -1,8 +1,29 @@
 #include "A2DConverter.h"
 
-bool ConvertAnalogCurrentRangesToDigital()
+bool ConvertAnalogCurrentRangesToDigital(int *AnalogCurrentRanges, int *DigitalCurrentRanges, int TotalNoOfCurrentRanges)
 {
   bool allCurrentRangesConverted = true;
+  int currentRangeIndex = 0;
+  float convertedValue = 0;
+  
+  for(currentRangeIndex = 0; currentRangeIndex < TotalNoOfCurrentRanges, currentRangeIndex++)
+  {
+    if(AnalogCurrentRanges[currentRangeIndex] > 4094)
+    {
+      printf("Invalid current reading");
+      allCurrentRangesConverted = false;
+    }
+  }
+  
+  if(allCurrentRangesConverted == true)
+  {
+    for(currentRangeIndex = 0; currentRangeIndex < TotalNoOfCurrentRanges, currentRangeIndex++)
+    {
+      convertedValue = ((AnalogCurrentRanges[currentRangeIndex] * 10) / 4094);
+      DigitalCurrentRanges[currentRangeIndex] = round(convertedValue);
+    }
+  }
+  
   
   return allCurrentRangesConverted;
 }
