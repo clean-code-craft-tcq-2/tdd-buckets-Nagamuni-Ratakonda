d0f856413d5d4ec77fd3f6058ddb3a36cea1d24a
Nagamuni-Ratakonda <91056036+Nagamuni-Ratakonda@users.noreply.github.com>
2022-04-14 10:47:11 +0530

    Update A2DConverter.h

--- A2DConverter.h
+++ A2DConverter.h
@@ -1,1 +1,3 @@
 bool ConvertAnalogCurrentRangesToDigital(int *AnalogCurrentRanges, int *DigitalCurrentRanges, int TotalNoOfCurrentRanges);
+void A2DConversion(int *AnalogCurrentRanges, int *DigitalCurrentRanges, int TotalNoOfCurrentRanges);
+bool ValidateAnalogCurrentRanges(int *AnalogCurrentRanges, int TotalNoOfCurrentRanges);
